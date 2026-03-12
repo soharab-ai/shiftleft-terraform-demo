a5424c947b8ae026a519993f50e052a16270e417
Harness SAST and SCA <noreply@shiftleft.io>
2026-03-12 09:33:56 +0000

    Fixing terraform/iam.tf for finding 58

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -13,28 +13,121 @@ resource "aws_iam_access_key" "user" {
   user = aws_iam_user.user.name
 }
 
-resource "aws_iam_user_policy" "userpolicy" {
-  name = "excess_policy"
-  user = "${aws_iam_user.user.name}"
+resource "aws_iam_policy" "privilege_escalation_boundary" {
+  name        = "privilege_escalation_boundary"
+  description = "Permissions boundary to prevent privilege escalation"
 
-  policy = <<EOF
-{
-  "Version": "2012-10-17",
-  "Statement": [
-    {
-      "Action": [
-        "ec2:*",
-        "s3:*",
-        "lambda:*",
-        "cloudwatch:*"
-      ],
-      "Effect": "Allow",
-      "Resource": "*"
-    }
-  ]
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "DenyPrivilegeEscalation"
+        Effect = "Deny"
+        Action = [
+          "iam:CreateAccessKey",
+          "iam:CreateLoginProfile",
+          "iam:UpdateLoginProfile",
+          "iam:AttachUserPolicy",
+          "iam:AttachGroupPolicy",
+          "iam:AttachRolePolicy",
+          "iam:PutUserPolicy",
+          "iam:PutGroupPolicy",
+          "iam:PutRolePolicy",
+          "iam:CreatePolicyVersion",
+          "iam:SetDefaultPolicyVersion",
+          "iam:AddUserToGroup",
+          "iam:UpdateAssumeRolePolicy",
+          "iam:DeleteUserPermissionsBoundary",
+          "iam:DeleteRolePermissionsBoundary",
+          "lambda:UpdateFunctionConfiguration",
+          "lambda:UpdateFunctionCode",
+          "lambda:CreateFunction",
+          "ec2:RunInstances",
+          "ec2:ModifyInstanceAttribute",
+          "sts:AssumeRole"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowReadOnlyOperations"
+        Effect = "Allow"
+        Action = [
+          "ec2:Describe*",
+          "s3:Get*",
+          "s3:List*",
+          "lambda:Get*",
+          "lambda:List*",
+          "cloudwatch:Get*",
+          "cloudwatch:Describe*",
+          "cloudwatch:List*"
+        ]
+        Resource = "*"
+      }
+    ]
+  })
 }
-EOF
+
+resource "aws_iam_user_policy" "userpolicy" {
+  name = "restricted_policy"
+  user = aws_iam_user.user.name
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid    = "AllowSpecificReadOperations"
+        Effect = "Allow"
+        Action = [
+          "ec2:DescribeInstances",
+          "ec2:DescribeVolumes",
+          "ec2:DescribeSecurityGroups",
+          "cloudwatch:GetMetricData",
+          "cloudwatch:DescribeAlarms",
+          "cloudwatch:ListMetrics"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowS3ReadOperations"
+        Effect = "Allow"
+        Action = [
+          "s3:GetObject",
+          "s3:ListBucket"
+        ]
+        Resource = [
+          "arn:aws:s3:::*",
+          "arn:aws:s3:::*/*"
+        ]
+      },
+      {
+        Sid    = "AllowLambdaReadOperations"
+        Effect = "Allow"
+        Action = [
+          "lambda:GetFunction",
+          "lambda:ListFunctions"
+        ]
+        Resource = "*"
+      },
+      {
+        Sid    = "AllowPassRoleToSpecificServices"
+        Effect = "Allow"
+        Action = [
+          "iam:PassRole"
+        ]
+        Resource = "*"
+        Condition = {
+          StringEquals = {
+            "iam:PassedToService" = [
+              "ec2.amazonaws.com",
+              "lambda.amazonaws.com"
+            ]
+          }
+        }
+      }
+    ]
+  })
 }
+
 
 output "username" {
   value = aws_iam_user.user.name
